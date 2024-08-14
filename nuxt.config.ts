196d8d510ed1096d91d22ed13d5242c941887411
alexoesmith <asmith@oomphworks.com>
2024-08-14 11:35:03 +0100

    Update site name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   site: {
-    name: "Lyles Coaches",
+    name: "A Lyles & Son",
     defaultLocale: "en",
   },
 
