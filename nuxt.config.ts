8850a84897cd81fa5fb16ff9b5daf3a28508c674
alexoesmith <asmith@oomphworks.com>
2024-10-25 09:29:43 +0100

    Config updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   site: {
-    name: "A Lyles & Son",
+    name: "A. Lyles and Son",
+    description: "Coach hire Wakefield from A. Lyles and Son",
     defaultLocale: "en",
   },
 
